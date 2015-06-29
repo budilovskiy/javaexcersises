7a1f753af3d256ee791194d89c6e30d309d92d97
budilovskiy <maksim.budilovskiy@gmail.com>
2015-06-29 11:55:41 +0300

    Create PerfectNumberList.java

---
+++ basic/exersises/difficult/Number-Theory/PerfectNumberList/PerfectNumberList.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		App test = new App();
+		
+		System.out.print("Enter the upper bound: ");
+		int number = scanner.nextInt();
+		
+		int perfectNumbers = test.calcPerfect(number);
+		double perfectPercent = ((double)perfectNumbers/number) * 100;
+		System.out.println("\n[" + perfectNumbers + " perfect numbers found (" + perfectPercent + "%)]\n");
+		
+		int notDeficient = test.calcNotDeficient(number);
+		double notDefPrcent = ((double)notDeficient/number) * 100;
+		System.out.println("\n[" + notDeficient + " perfect numbers found (" + notDefPrcent + "%)]\n");
+	}
+}
