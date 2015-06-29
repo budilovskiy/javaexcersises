e500662d81b9b5da2c12b44d3112a645c61d76a8
budilovskiy <maksim.budilovskiy@gmail.com>
2015-06-29 13:40:41 +0300

    Create Test.java

---
+++ basic/exersises/NumberTheory/PrimeFactors/Test.java
@@ -0,0 +1,21 @@
+package basic.exersises.numbertheory;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		App test = new App();
+
+		System.out.print("Enter the upper bound: ");
+		int number = scanner.nextInt();
+		scanner.close();
+
+		int primeFactors = test.productOfPrimeFactorsCounting(number);
+		double primesPercent = ((double) primeFactors / number) * 100;
+		System.out.println("\n[" + primeFactors + " numbers found ("
+				+ primesPercent + "%)]\n");
+
+	}
+}
