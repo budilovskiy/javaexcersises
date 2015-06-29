673bb3d1a469ce59574bb63c31984aa66ad63135
budilovskiy <maksim.budilovskiy@gmail.com>
2015-06-29 12:35:03 +0300

    Create Test.java

---
+++ basic/exersises/NumberTheory/Primes/Test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		App test = new App();
+		
+		System.out.print("Enter the upper bound: ");
+		int number = scanner.nextInt();
+		scanner.close();
+		
+		int primeNumbers = test.primeNumbers(number);
+		double primesPercent = ((double)primeNumbers/number) * 100;
+		System.out.println("\n[" + primeNumbers + " prime numbers found (" + primesPercent + "%)]\n");
+		
+	}
+}
